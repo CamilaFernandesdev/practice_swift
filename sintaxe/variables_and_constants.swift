d7e9a826b9624de76df3e728329058644565fae5
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-09-25 21:26:09 -0300

    Create variables_and_constants.swift

---
+++ sintaxe/variables_and_constants.swift
@@ -0,0 +1,16 @@
+// Variables and Constants
+
+let maximusNumber = 10
+let maximusNumber2 = "10"
+var currentWidth = 0
+
+// Type annotations
+
+var welcomeMessage: String 
+welcomeMessage = 'Hello'
+
+var x, y, z: Double
+
+print(welcomeMessage)
+print("The number maximus is \(maximusNumber)")
+print(maximusNumber2, terminator="")
